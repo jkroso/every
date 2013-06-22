b7ec468bde19cdedf7aef5cc94628cf452fbb242
jkroso <jkroso@gmail.com>
2013-06-22 19:36:49 +1200

    decorate

--- async.js
+++ async.js
@@ -1,24 +1,28 @@
 
 var each = require('foreach/async').plain
-var ResultType = require('result-type')
+  , ResultType = require('result-type')
+  , decorate = require('when/decorate')
+
+module.exports = decorate(parallelEvery)
+module.exports.plain = parallelEvery
 
 /**
  * assert `pred` on every key value pair of `obj`
  *
  * @param {Object|Array} obj
- * @param {Function} pred (value, key) -> Promise Boolean
+ * @param {Function} pred (value, key)
  * @param {Any} [ctx]
  * @return {Result} Boolean
  */
 
-module.exports = function(obj, pred, ctx){
+function parallelEvery(obj, pred, ctx){
 	if (obj == null) return true
 	var res = true
 	return each(obj, function(value, key){
 		if (!res) return
 		var ok = pred.call(this, value, key)
 		if (ok instanceof ResultType) return ok.then(assert)
-		else if (!ok) res = false
+		else assert(ok)
 	}, ctx).then(function(){
 		return res
 	})
